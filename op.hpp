ccc3dadc02b8da4d085a46715fbc633d6dfa2d37
awang923 <awang144@ucr.edu>
2020-11-01 15:34:53 -0800

    googletest

--- op.hpp
+++ op.hpp
@@ -2,32 +2,33 @@
 #define __OP_HPP__
 
 #include "base.hpp"
+using namespace std;
 
 class Op : public Base {
     public:
-        Op(double value) : Base() { }
+        Op(double value) : Base() { };
         virtual double evaluate() { return 0.0; }
         virtual std::string stringify() { return ""; }
 };
 class NegOpMock: public Base{
         public:
                 NegOpMock() {};
-                virtual double evaluate(){return -7.5}
-                virtual string stringify() {return "-7.5"}
-}
+                virtual double evaluate(){return -7.5;}
+                virtual string stringify() {return "-7.5";}
+};
 
 class TripleOpMock: public Base{
         public:
-                NegOpMock() {};
-                virtual double evaluate(){return 312.123}
-                virtual string stringify() {return "312.123"}
-}
+                TripleOpMock() {};
+                virtual double evaluate(){return 312.123;}
+                virtual string stringify() {return "312.123";}
+};
 
 class IntOpMock: public Base{
         public:
-                NegOpMock() {};
-                virtual double evaluate(){return 1}
-                virtual string stringify() {return "1"}
-}
+                IntOpMock() {};
+                virtual double evaluate(){return 1;}
+                virtual string stringify() {return "1";}
+};
 
 #endif //__OP_HPP__
