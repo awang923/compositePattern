d0bb99251580e3b15006a5947f61548e46087bcd
awang923 <awang144@ucr.edu>
2020-10-31 21:04:31 -0700

    googletest and op.hpp

--- op.hpp
+++ op.hpp
@@ -9,5 +9,25 @@ class Op : public Base {
         virtual double evaluate() { return 0.0; }
         virtual std::string stringify() { return ""; }
 };
+class NegOpMock: public Base{
+        public:
+                NegOpMock() {};
+                virtual double evaluate(){return -7.5}
+                virtual string stringify() {return "-7.5"}
+}
+
+class TripleOpMock: public Base{
+        public:
+                NegOpMock() {};
+                virtual double evaluate(){return 312.123}
+                virtual string stringify() {return "312.123"}
+}
+
+class IntOpMock: public Base{
+        public:
+                NegOpMock() {};
+                virtual double evaluate(){return 1}
+                virtual string stringify() {return "1"}
+}
 
 #endif //__OP_HPP__
