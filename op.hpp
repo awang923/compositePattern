bef784c65b67e9311a8aa847354331fc480734af
jsegu447 <jsegu010@ucr.edu>
2020-10-31 21:33:05 -0700

    op class implemented

--- op.hpp
+++ op.hpp
@@ -1,42 +1,45 @@
 #ifndef __OP_HPP__
 #define __OP_HPP__
-
 #include "base.hpp"
+#include <string>
+using namespace std;
 
 class Op : public Base {
-    public:
-        Op(double value) : Base() { }
-        virtual double evaluate() { return 0.0; }
-        virtual std::string stringify() { return ""; }
+ private:
+ double val;  
+ public:
+        Op(double value) : Base() {val = value; }
+        virtual double evaluate() { return val; }
+        virtual std::string stringify() { return to_string(val); }
 };
 
 class SevenOpMock: public Base{
         public:
                 SevenOpMock() {};
-                virtual double evaluate(){return 7.5}
-                virtual string stringify() {return "7.5"}
-}
+                virtual double evaluate(){return 7.5;}
+                virtual string stringify() {return "7.5";}
+};
 
 class NegOpMock: public Base{
         public:
                 NegOpMock() {};
-                virtual double evaluate(){return -7.5}
-                virtual string stringify() {return "-7.5"}
-}
+                virtual double evaluate(){return -7.5;}
+                virtual string stringify() {return "-7.5";}
+};
 
 class TripleOpMock: public Base{
         public:
-                NegOpMock() {};
-                virtual double evaluate(){return 312.123}
-                virtual string stringify() {return "312.123"}
-}
+                TripleOpMock() {};
+                virtual double evaluate(){return 312.123;}
+                virtual string stringify() {return "312.123";}
+};
 
 class IntOpMock: public Base{
         public:
-                NegOpMock() {};
-                virtual double evaluate(){return 1}
-                virtual string stringify() {return "1"}
-}
+                IntOpMock() {};
+                virtual double evaluate(){return 1;}
+                virtual string stringify() {return "1";}
+};
 
 
 #endif //__OP_HPP__
