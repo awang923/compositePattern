0a9a46149719162a075a25cb1433b8d7be97a450
jsegu447 <jsegu010@ucr.edu>
2020-10-31 21:07:56 -0700

    Added testing mocks

--- op.hpp
+++ op.hpp
@@ -10,4 +10,33 @@ class Op : public Base {
         virtual std::string stringify() { return ""; }
 };
 
+class SevenOpMock: public Base{
+        public:
+                SevenOpMock() {};
+                virtual double evaluate(){return 7.5}
+                virtual string stringify() {return "7.5"}
+}
+
+class NegOpMock: public Base{
+        public:
+                NegOpMock() {};
+                virtual double evaluate(){return -7.5}
+                virtual string stringify() {return "-7.5"}
+}
+
+class TripleOpMock: public Base{
+        public:
+                NegOpMock() {};
+                virtual double evaluate(){return 312.123}
+                virtual string stringify() {return "312.123"}
+}
+
+class IntOpMock: public Base{
+        public:
+                NegOpMock() {};
+                virtual double evaluate(){return 1}
+                virtual string stringify() {return "1"}
+}
+
+
 #endif //__OP_HPP__
