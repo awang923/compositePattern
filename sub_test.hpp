7c5006984eae25f1e0ad81b5b010480184944f72
jsegu447 <jsegu010@ucr.edu>
2020-11-02 21:32:07 -0800

    added test cases for classes

--- sub_test.hpp
+++ sub_test.hpp
@@ -11,7 +11,25 @@ TEST(SubTest, SubEvaluateNonZero) {
     Sub* insideTest = new Sub(test1,test2);
     Sub* testend = new Sub(test2,insideTest);
     EXPECT_EQ(testend->evaluate(), 6);
+    EXPECT_EQ(testend->stringify(),"7.000000 - 8.000000 - 7.000000");
 }
+
+TEST(SubTest, SubOp) {
+    Op* test1 = new Op(8);
+    Op* test2 = new Op(7);
+    Sub* testend = new Sub(test1,test2);
+    EXPECT_EQ(testend->evaluate(), 1);
+    EXPECT_EQ(testend->stringify(),"8.000000 - 7.000000");
+}
+
+TEST(SubTest, SubOpZero) {
+    Op* test1 = new Op(8);
+    Sub* testend = new Sub(test1,test1);
+    EXPECT_EQ(testend->evaluate(), 0);
+    EXPECT_EQ(testend->stringify(),"8.000000 - 8.000000");
+}
+
+
 
 #endif //__SUB_TEST_HPP__
 
